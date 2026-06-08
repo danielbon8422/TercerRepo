b0809fa117f3c3ca0eeb9833cfdf5e1365856ed7
danielbon <daniel_bon84@hotmail.com>
2026-06-08 10:47:20 +0200

    PrimerLanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Daniel Bonilla",                         # Tu nombre
+    author_email="daniel.bonilla@atmira.com",                 # Tu correo electrónico
+    url="https://github.com/danielbon8422/tercerRepo",     # URL del proyecto
+)
